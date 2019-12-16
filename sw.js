1dce0ee852c20ecd2a40ccbd5f3ce15b53863f0a
Ronald Montealegre <ronaldmonte@gmail.com>
2019-12-16 13:43:34 -0500

    modificaciones iphoone

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const CACHE_STATIC='static-v3';
-const CACHE_DYNAMIC='dynamic-v1';
+const CACHE_STATIC='static-v4';
+const CACHE_DYNAMIC='dynamic-v2';
 const CACHE_INMUTABLE='inmutable-v1';
 
 const APP_SHELL = [
@@ -44,6 +44,10 @@ self.addEventListener('activate', e => {
     const respuesta=caches.keys().then(keys => {
         keys.forEach(key => {
             if(key !== CACHE_STATIC && key.includes('static')) {
+                return caches.delete(key);
+            }
+
+            if(key !== CACHE_DYNAMIC && key.includes('dynamic')) {
                 return caches.delete(key);
             }
 
